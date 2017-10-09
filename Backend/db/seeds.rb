fa0db4a370aacd5f43a307d1b61d7f804b008e66
Peter Ludlum <t.broeck@gmail.com>
2017-10-08 18:36:50 -0700

    created users and session controller

--- Backend/db/seeds.rb
+++ Backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+test_user = User.new(email: "test@gmail.com", password: "password")
+
+test_user.create

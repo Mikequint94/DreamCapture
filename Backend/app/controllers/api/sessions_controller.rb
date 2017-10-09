cecbaa81309a7c6be04e08774753a90edf4362de
Peter Ludlum <t.broeck@gmail.com>
2017-10-09 10:36:47 -0700

    bug fixes for session controller

--- Backend/app/controllers/api/sessions_controller.rb
+++ Backend/app/controllers/api/sessions_controller.rb
@@ -1,7 +1,7 @@
-class Api::SessionController < ApplicationController
+class Api::SessionsController < ApplicationController
   def create
     @user = User.find_by_credentials(
-                params[:user][:username],
+                params[:user][:email],
                 params[:user][:password])
     if @user
       login(@user)
--- Backend/test/controllers/static_pages_controller_test.rb
+++ Backend/test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Api::SessionControllerTest < ActionDispatch::IntegrationTest
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end

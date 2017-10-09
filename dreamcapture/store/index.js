9adc9c383c417207c70754541535552839cd0b07
Adrienne Lin <lin.adrienne@gmail.com>
2017-10-08 19:47:09 -0700

    Add structure, store

---
+++ dreamcapture/store/index.js
@@ -0,0 +1,27 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import RootReducer from '../reducers/root_reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {AsyncStorage} from 'react-native';
+import {persistStore, autoRehydrate} from 'redux-persist';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(
+    RootReducer,
+    preloadedState,
+    composeWithDevTools(
+      applyMiddleware(thunk, ...middlewares),
+      autoRehydrate()
+    ));
+  persistStore(store, {storage: AsyncStorage});
+  return store;
+};
+
+export default configureStore;

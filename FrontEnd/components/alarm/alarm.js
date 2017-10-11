0de317c762b0e0cd92b9693b7b53d64c007c0c77
Peter Ludlum <t.broeck@gmail.com>
2017-10-10 17:32:06 -0700

    added RNCalendarEvents library to alarm

--- FrontEnd/components/alarm/alarm.js
+++ FrontEnd/components/alarm/alarm.js
@@ -4,14 +4,52 @@ import {
   Text,
   View
 } from 'react-native';
+import RNCalendarEvents from 'react-native-calendar-events';
 
 export default class AlarmScreen extends React.Component {
   static navigationOptions = {
       title: 'Alarm',
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cal_auth: ''
+    };
+  }
+
+  componentDidMount() {
+    RNCalendarEvents.authorizationStatus()
+    .then(status => {
+      this.setState({ cal_auth: status })
+      if(status === 'undetermined') {
+        
+      }
+    })
+  }
+
+
+
   render() {
     return (
-      <Text> Alarm Screen </Text>
+      <View style={styles.container}>
+        <Text> Alarm Screen </Text>
+        <Text> {this.state.cal_auth} </Text>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});

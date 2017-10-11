934d3c710b3dc16b726728b1abd41cef40834ec4
Peter Ludlum <t.broeck@gmail.com>
2017-10-11 11:08:12 -0700

    added permissions to ios calendar

--- FrontEnd/components/alarm/alarm.js
+++ FrontEnd/components/alarm/alarm.js
@@ -4,6 +4,8 @@ import {
   Text,
   View
 } from 'react-native';
+
+import TimerMixin from 'react-timer-mixin';
 import RNCalendarEvents from 'react-native-calendar-events';
 
 export default class AlarmScreen extends React.Component {
@@ -14,18 +16,48 @@ export default class AlarmScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cal_auth: ''
+      cal_auth: '',
+      time: '',
+      date: '',
+      events: '',
     };
+    this.time = new Date;
+
+    this.updateTime = this.updateTime.bind(this);
   }
 
   componentDidMount() {
     RNCalendarEvents.authorizationStatus()
-    .then(status => {
-      this.setState({ cal_auth: status })
-      if(status === 'undetermined') {
-        
-      }
-    })
+      .then(status => {
+        this.setState({ cal_auth: status })
+        if(status === 'undetermined') {
+          RNCalendarEvents.authorizeEventStore()
+          .then((newStatus) => {
+            if(newStatus == 'authorized') {
+              this.setState({ cal_auth: newStatus })
+            }
+          })
+        }
+      })
+      .catch(error => console.warn('Auth Error: ', error));
+
+
+    this.updateTime();
+    clockId = setInterval(this.updateTime, 1000);
+
+  }
+
+  componentWillUnmount() {
+    clearInterval(clockId);
+  }
+
+
+
+  updateTime() {
+    this.time = new Date();
+    this.setState({time: this.time.toTimeString(),
+                   date: this.time.toDateString()
+                 });
   }
 
 
@@ -35,6 +67,9 @@ export default class AlarmScreen extends React.Component {
       <View style={styles.container}>
         <Text> Alarm Screen </Text>
         <Text> {this.state.cal_auth} </Text>
+        <Text> {this.state.time.slice(0, 8)} </Text>
+        <Text> {this.state.date} </Text>
+        <Text> {this.state.events} </Text>
       </View>
     )
   }

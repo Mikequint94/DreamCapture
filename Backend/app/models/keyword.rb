de5eb67d87c1d4081c5f6e90b091271a140cf2ac
Peter Ludlum <t.broeck@gmail.com>
2017-10-12 08:52:31 -0700

    fixed create action for keywords, added many-to-many association between user and keyword, keyword index now returns user-specific list

--- Backend/app/models/keyword.rb
+++ Backend/app/models/keyword.rb
@@ -6,7 +6,9 @@ class Keyword < ApplicationRecord
   has_many :taggings, dependent: :destroy
 
   has_many :dreams, through: :taggings
-  
+
+  has_many :users, through: :dreams
+
   # ----- Methods -------
 
 end
